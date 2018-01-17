9ec555a0d3878d92d1fe7ec42679080ef559e48f
tyler <ty_sany@163.com>
2018-01-18 01:58:21 +0800

    Merge branch 'master' of gitee.com:tangyongsungy/filter

--- app/src/androidTest/java/com/sny/filter/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/sny/filter/ExampleInstrumentedTest.java
@@ -3,6 +3,7 @@ package com.sny.filter;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,11 +17,32 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+//    @Test
+//    public void useAppContext() throws Exception {
+//        // Context of the app under test.
+//        Context appContext = InstrumentationRegistry.getTargetContext();
+//
+//        assertEquals("com.sny.filter", appContext.getPackageName());
+//    }
 
-        assertEquals("com.sny.filter", appContext.getPackageName());
+    @Test
+    public void random() throws Exception {
+
+        int Atype = 0;
+        int BType = 0;
+
+        int count = 10000;
+        while (count > 0) {
+            if (Math.random() * 100 > 30) {
+                BType++;
+            } else {
+                Atype++;
+            }
+            count--;
+        }
+        Log.i("tyler.tang", "Atype:\t" + Atype + "-- Btype:\t" + BType);
+
+        assertTrue(BType > 30);
     }
+
 }
--- app/src/test/java/com/sny/filter/ExampleUnitTest.java
+++ app/src/test/java/com/sny/filter/ExampleUnitTest.java
@@ -1,5 +1,8 @@
 package com.sny.filter;
 
+import android.icu.text.MessagePattern;
+import android.util.Log;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +13,25 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void randomone() throws Exception {
+
+        int Atype = 0;
+        int BType = 0;
+
+        int count = 10000;
+        while (count > 0) {
+            if (Math.random() * 100 > 30) {
+                BType++;
+            } else {
+                Atype++;
+            }
+            count--;
+        }
+        Log.i("tyler.tang", "Atype:\t" + Atype + "-- Btype:\t" + BType);
+
+        assertTrue(BType > 30);
     }
+
 }
